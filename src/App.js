c24dbb462603b37770e4f71ba7c88a82467a17a4
Igor Magalhães <igortrapp30@gmail.com>
2022-09-01 08:51:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
+  const [movies, getMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const filteredMovieData = data.results.map((data) => {
+      return {
+        id: data.episode_id,
+        title: data.title,
+        openingText: data.opening_crawl,
+        releaseDate: data.release_date,
+      };
+    });
+    getMovies(filteredMovieData);
+    setIsLoading(false);
+  }
+
+  function clearMoviesHandler() {
+    getMovies((prev) => []);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={clearMoviesHandler}>Clear Movies</button>
+      </section>
+
+      <section>
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
   );
 }
 
