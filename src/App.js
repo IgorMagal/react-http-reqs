43847e5c40aad0a1a832e7e9664dd5cc773fe960
Igor Magalhães <igortrapp30@gmail.com>
2022-09-03 10:01:28 -0400

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,44 +1,106 @@
 import React, { useState } from "react";
-
+import AddMovie from "./components/AddMovie";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import { useEffect } from "react";
+import { useCallback } from "react";
 
 function App() {
   const [movies, getMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-http-reqs-f9475-default-rtdb.firebaseio.com/movies.json"
+      );
 
-    const filteredMovieData = data.results.map((data) => {
-      return {
-        id: data.episode_id,
-        title: data.title,
-        openingText: data.opening_crawl,
-        releaseDate: data.release_date,
-      };
-    });
-    getMovies(filteredMovieData);
+      if (!response.ok) {
+        throw new Error("ERROR: Ops... Could not fetch any movies!");
+      }
+      const data = await response.json();
+
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      getMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   function clearMoviesHandler() {
     getMovies((prev) => []);
   }
 
+  const addMovieHandler = (movie) => {
+    fetch(
+      "https://react-http-reqs-f9475-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => fetchMoviesHandler());
+  };
+
+  let content;
+  if (isLoading) {
+    content = (
+      <p>
+        <strong>Loading...</strong>
+      </p>
+    );
+  }
+
+  if (!isLoading && error) {
+    content = (
+      <p>
+        <strong>{error}</strong>
+      </p>
+    );
+  }
+
+  if (movies.length <= 0 && !isLoading) {
+    content = (
+      <p>
+        <strong>Your list is empty, try to fetch some more movies!</strong>
+      </p>
+    );
+  }
+
+  if (movies.length > 0 && !isLoading) {
+    content = <MoviesList movies={movies} />;
+  }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
         <button onClick={clearMoviesHandler}>Clear Movies</button>
       </section>
-
-      <section>
-        {!isLoading && <MoviesList movies={movies} />}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
